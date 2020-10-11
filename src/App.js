fcf49e3891b0c5d50bbb4006919ff9b6f173b89c
mikehaus <mike.haus.design@gmail.com>
2020-10-11 13:04:36 -0600

    Added React Router for nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from 'react-router-dom';
 import MainContainer from './components/maincontainer';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <MainContainer />      
-    </div>
+    <Router>
+      <div>
+        <MainContainer />  
+      </div>
+    </Router>
   );
 }
 
--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import '../styles/login.css';
-import keys from '../api/apikeys';
+import { keys } from '../api/apikeys';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
 export default function Login(props) {
     const [error, setError] = useState(null);
     const [loaded, setLoaded] = useState(false);
+
+    setLoaded = () => {
+        loaded = true;
+    }
+
+    const client_id = keys.client_id;
+    const secret = keys.secret;
     
     const userLogin = () => {
-        console.log('hello');
+        console.log('Authenticating login...');
+        fetch('https://accounts.spotify.com/authorize')
+            .then(
+                (result) => {
+                    
+                }
+            )
     }
 
     return (

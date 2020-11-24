30ee5d2d5f7bb1afd94fdcbd54c7583eee640647
mikehaus <mike.haus.design@gmail.com>
2020-11-24 16:02:07 -0700

    Added song table basic css. Deciding how to implement

---
+++ src/components/maincontent/buildingblocks/songtable.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+function SongTable(props) {
+    <div>
+        <section>
+            
+        </section>
+    </div>
+}
+
+export default SongTable;
